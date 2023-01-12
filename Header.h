59eef8e400a0bdeadaffbe174281dbbbe3013a75
BobLetuce <121960154+BobLetuce@users.noreply.github.com>
2023-01-11 19:26:52 -0500

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
+int attackc1(void);
+int enemyattack(void);
+int attackc2(void);
+int critc1(void);
+int critslevel(int critlevel);
+int critc2(void);
+int attackslevel(int attacklevel);
